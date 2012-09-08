555ef3f5983c7993227a5cc71c3896441a88de2a
Antoine Pelisse <apelisse@gmail.com>
2012-09-08 13:55:47 +0200

    tvsubs: Add docstrings
    Add docstring so that we can generate a nice documentation with
    epydoc.

--- tvsubtitles.py
+++ tvsubtitles.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+tvsubtitles
+
+This module can be used as an interface to TVSubtitles.net.
+
+Currently it uses some regex rather than html/xml parsing to find
+the values we need when downloading the pages.
+
+The main function to be called is L{download_subtitle}.
+"""
+
 import argparse
 import re
 import StringIO
@@ -8,13 +19,27 @@ import urllib
 import urlparse
 import zipfile
 
+__author__ = "Antoine Pelisse"
+__copyright__ = "Copyright 2012, Antoine Pelisse"
+__license__ = "BSD"
+__version__ = "0.9"
+__contact__ = "apelisse@gmail.com"
 
 BASE_URL = 'http://www.tvsubtitles.net'
 
 
 def search_tvshow(tvshow):
     """
-    Return the list of series found, with matching id
+    Search for a sire according to tvshow name.
+
+    This returns the same list as it would have been returned by
+    U{tvsubtitles.net}. It means that there is B{no processing} on the data.
+    It also means that you should not only rely on this and you will probably
+    have many stupid results in there as their search engine is I{very}
+    permissive.
+
+    @param tvshow: Name of the tvshow
+    @return: List of tuples: tvshow id, tvshow name
     """
     tvshow = str(tvshow)
 
@@ -33,6 +58,14 @@ def search_tvshow(tvshow):
 
 
 def search_episode(tvshowid, season, episode):
+    """
+    Searches for a specific episode
+
+    @param tvshowid: Id of the tvshow in tvs database
+    @param season: Season number you are interested in
+    @param episode: Episode number you are looking for
+    @return: Id of the episode
+    """
     tvshowid = int(tvshowid)
     season = int(season)
     episode = int(episode)
@@ -51,7 +84,14 @@ def search_episode(tvshowid, season, episode):
 
 def search_subtitles(episodeid, language):
     """
-    Return a list of ids
+    Returns all subtitles available for a specific episode and language.
+
+    @todo: Add more information about subtitles, so we can select one
+    decently.
+
+    @param episodeid: Episode of interest
+    @param language: Language of the subtitles
+    @return: List of all Subtitles Id
     """
     episodeid = int(episodeid)
 
@@ -69,12 +109,27 @@ def search_subtitles(episodeid, language):
 
 
 def download_subid(subid):
+    """
+    Download the given subtitle
+
+    @param subid: Id of the subtitle to download
+    @return: Subtitle as text
+    """
     subid = int(subid)
 
     return _download_file(subid)
 
 
 def _download_subid(subid):
+    """
+    Download the given subtitle
+
+    @deprecated: It looks like tvs uses the subtitle id as file id, so
+    we don't need to get it, we already have that value !
+
+    @param subid: Id of the subtitle to download
+    @return: Subtitle as text
+    """
     subid = int(subid)
     search_path = urlparse.urljoin(
         self.base_url,
@@ -88,6 +143,12 @@ def _download_subid(subid):
 
 
 def _download_file(fileid):
+    """
+    Download Zip archive, look for subtitle file in it, open it and read it.
+
+    @param fileid: Id of the file to download
+    @return: Subtitle as text
+    """
     download_path = urlparse.urljoin(BASE_URL, "download-%d.html" % fileid)
 
     result = urllib.urlopen(download_path).read()
@@ -110,6 +171,20 @@ def _download_file(fileid):
 
 
 def download_subtitle(tvshow, season, episode, language):
+    """
+    This is a shortcut function to download a subtitle.
+
+    It actually runs most of the function of the file in the correct
+    order to make downloading of a subtitle easy.
+    Searches for the tvshow id, then searches for the episode id,
+    and then searches for the subtitle id, and then downloads the file and
+    reads it !
+
+    @param tvshow: Tvshow name
+    @param season: Season number
+    @param episode: Episode number
+    @param language: Language of the subtitle required
+    """
     tvshowid = search_tvshow(tvshow)
     episodeid = search_episode(tvshowid[0][0], season, episode)
     subid = search_subtitles(episodeid, language)
@@ -118,6 +193,17 @@ def download_subtitle(tvshow, season, episode, language):
 
 
 def main():
+    """
+    Main function
+
+    This function is called when the script is called by python directly.
+
+    It parses the arguments given on command line and call the
+    L{download_subtitle} function to get the subtitle.
+    You have to know every arguments of the function to use this script like
+    this: tvshow name, season number and episode number. It won't do
+    automatic calculation of what is the movie, etc.
+    """
     parser = argparse.ArgumentParser(
         description="Download subtitle for TV Episode")
     parser.add_argument('tvshow')
