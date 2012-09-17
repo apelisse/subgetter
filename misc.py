ecaf2499e3e21f6fc2d6aa6b41bfcc8912d2f1ad
Antoine Pelisse <apelisse@gmail.com>
2012-09-17 21:23:17 +0200

    misc: Add strings_contained function
    This function searches every word from reduced_name in the
    complete_name string.
    The idea is that, we can make sure that the title we want to validate
    is fully contained in the file name.
    For example:
    If we think the movie is named "The Shawshank Redemption", we will look
    for "The", "Shawshank", and "Redemption" in the filename.

--- misc.py
+++ misc.py
@@ -7,6 +7,8 @@ Currently, we only provide one function:
   - L{dice_coefficient}
 """
 
+import re
+
 
 def dice_coefficient(a, b, ignore_case=True):
     """
@@ -46,3 +48,24 @@ def dice_coefficient(a, b, ignore_case=True):
     dice_coeff = overlap * 2.0 / (len(a_bigrams) + len(b_bigrams))
 
     return dice_coeff
+
+
+def strings_contained(complete_name, reduced_name):
+    """
+    Look for strings in reduced_name in complete_name
+
+    The idea is to search for all the substrings of reduced_name inside
+    complete_name. Substrings of reduced_name are calculated by splitting
+    for each non-word character.
+    If all characters are found, it's quite a good sign that the name is close
+
+    @param complete_name: Name of the string to look in (most likely bigger
+    than the real string, with junk)
+    @param reduced_name: Name to look for, is it in the string
+    @return: Percentage of word found
+    @rtype: float
+    """
+    substrings = re.split('\W+', reduced_name)
+
+    return (len([1 for string in substrings if string in complete_name]) /
+            len(substrings))
